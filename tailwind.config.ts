6b1df18439b0ef155c8eb990ce493fe6dedf8d5a
kimtaehyung <xoguddkenl@gmail.com>
2024-10-09 20:14:20 +0900

    테스트 커밋

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,15 @@ const config: Config = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "orange-L2": "#DF7B07",
+        "orange-L1": "#F5D039",
+        "orange-L3": "#F0B920",
+        "apple-black": "#1d1d1f",
+        "apple-gray": "#86868B",
+      },
+    },
   },
   plugins: [],
 };
